b033d6fcd591b31f15203d09ed2f6ee839dc6f82
figen2024 <167141401+figen2024@users.noreply.github.com>
2024-04-23 15:24:04 -0400

    Add files via upload

---
+++ module_6_rev.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ex2
+# 
+# module_6_rev
+
+# ex3
+# 
+# Introduction
+# 
+# In this notebook, module 6 was reviewed
+
+# ex4 - data science languages
+# 
+# 1. python
+# 2. r
+# 3. r studio
+# 4. cc+
+
+# ex 5 - data science libraries
+# 1. numpy
+# 2. pandas
+# 3. ternsorflow
+# 4. matplotlib
+
+# ex6 - table
+# 
+# Data Science Tools
+# |Name of the Tool|
+# |----------------|
+# |Python|
+# |r|
+# |Jupyter notebook|
+# 
+
+# ex7 - heading
+# 
+# <h3 style="color: red; font-size: 22px;">Examples of evaluating arithmetic expressions in Python</h3>
+
+# In[6]:
+
+
+#ex 8
+
+7+13-3
+
+
+# In[8]:
+
+
+#ex-9
+#minutes to hours
+#255 minutes
+255/60
+
+
+# hours
+
+# <h1 style="color: black; font-size: 16px;">ex 10 objective list</h1>
+
+# Objectives:
+# 
+# *List popular languages for Data Science
+# 
+# *Python libraries
+# 
+# *Data science tools
+
+# ex11
+# <h3 style="color: red; font-size: 16px;">author</h3>
+# <h3 style="color: blue; font-size: 14px;">figen</h3>
+
+# https://github.com/figen2024/exercisemodule6.git
+# 
